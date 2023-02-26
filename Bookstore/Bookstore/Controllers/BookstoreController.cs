850ab9dadc461081fdbadeb9fe420287ae770c41
MatthewConnelly001 <42534260+MatthewConnelly001@users.noreply.github.com>
2023-02-27 02:18:19 +1100

    Created BookstoreController and added test public methods.

---
+++ Bookstore/Bookstore/Controllers/BookstoreController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Bookstore.Controllers
+{
+    public class BookstoreController : Controller
+    {
+        /*
+        public IActionResult Index()
+        {
+            return View();
+        }
+        */
+        //
+        // GET: /Bookstore/
+        public string Index()
+        {
+            return "Bookstore index page...";
+        }
+        //
+        // GET: /Bookstore/Welcome/
+        // Requires using System.Text.Encodings.Web;
+        // Uses 'name' as a URL query parameter.
+        public string Welcome(string name)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}. This is the Bookstore welcome page.");
+        }
+
+    }
+}
+
+
