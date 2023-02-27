db940c6817d4d87564c83898fde329828ae6906b
MatthewConnelly001 <42534260+MatthewConnelly001@users.noreply.github.com>
2023-02-28 01:29:02 +1100

    Added View/Bookstore/Index.cshtml view with test html content. Changed BookstoreController to present this.

--- Bookstore/Bookstore/Controllers/BookstoreController.cs
+++ Bookstore/Bookstore/Controllers/BookstoreController.cs
@@ -5,18 +5,19 @@ namespace Bookstore.Controllers
 {
     public class BookstoreController : Controller
     {
-        /*
         public IActionResult Index()
         {
             return View();
         }
-        */
+
+        /*
         //
         // GET: /Bookstore/
-        public string Index()
+        public IActionResult Index()
         {
-            return "Bookstore index page...";
+            return View();
         }
+
         //
         // GET: /Bookstore/Welcome/
         // Requires using System.Text.Encodings.Web;
@@ -25,7 +26,7 @@ namespace Bookstore.Controllers
         {
             return HtmlEncoder.Default.Encode($"Hello {name}. This is the Bookstore welcome page.");
         }
-
+        */
     }
 }
 
